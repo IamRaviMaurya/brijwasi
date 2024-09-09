9e1aa5da89e01a3356218886f589b328c3d41492
IamRaviMaurya <82931953+IamRaviMaurya@users.noreply.github.com>
2024-09-09 15:51:10 +0530

    changes update

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,16 +1,34 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import logo from "../../Assets/radha-krishna.png";
 import cart_icon from "../../Assets/cart-icon.png";
 import nav_menu from "../../Assets/nav-menu.png";
-import './NavBar.css'
+import "./NavBar.css";
 
 const NavBar = () => {
   const menuRef = useRef();
+  const dropdownRef = useRef();
+
   const dropdownToggle = (e) => {
     menuRef.current.classList.toggle("nav-menu-visible");
     e.target.classList.toggle("open");
   };
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        menuRef.current &&
+        !menuRef.current.contains(event.target) &&
+        !dropdownRef.current.contains(event.target)
+      ) {
+        menuRef.current.classList.remove("nav-menu-visible");
+        dropdownRef.current.classList.remove("open");
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
   return (
     <div className="navbar">
       <div className="nav-logo">
@@ -19,6 +37,7 @@ const NavBar = () => {
       </div>
       <img
         onClick={dropdownToggle}
+        ref={dropdownRef}
         src={nav_menu}
         alt=""
         className="nav-dropdown"
