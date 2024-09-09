41f2dd1f4ac4b839a2d78511e242e2802354477d
IamRaviMaurya <82931953+IamRaviMaurya@users.noreply.github.com>
2024-09-09 15:44:49 +0530

    worked on mobile side responsive

--- src/Assets/Json Data/sweets.js
+++ src/Assets/Json Data/sweets.js
@@ -48,6 +48,48 @@ let data_product = [
     image: p6_img,
     new_price: 100.0,
     old_price: 150.0,
+  },
+  {
+    id: 1,
+    name: "Boondi Laddu bits of fried and sugar-soaked batter",
+    image: p1_img,
+    new_price: 50.0,
+    old_price: 80.5,
+  },
+  {
+    id: 2,
+    name: "Gulab jamun Indian dessert of fried dough balls soaked in a sweet, floral syrup",
+    image: p2_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 3,
+    name: "Jalebi South Asian dessert that's crispy on the outside, chewy on the inside, and full of sticky syrup",
+    image: p3_img,
+    new_price: 60.0,
+    old_price: 100.5,
+  },
+  {
+    id: 4,
+    name: "Khow Peda thick, semi-soft round shape",
+    image: p4_img,
+    new_price: 100.0,
+    old_price: 150.0,
+  },
+  {
+    id: 5,
+    name: "Rasgulla made from milk-based dumplings cooked in sugar syrup",
+    image: p5_img,
+    new_price: 100.0,
+    old_price: 150.0,
+  },
+  {
+    id: 6,
+    name: "Soan papdi its crumbly, crunchy texture",
+    image: p6_img,
+    new_price: 100.0,
+    old_price: 150.0,
   }
   
 ];
