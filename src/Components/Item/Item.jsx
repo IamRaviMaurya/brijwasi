d12b4dcfbe1498f476aab3122668b496d34ac8a4
IamRaviMaurya <82931953+IamRaviMaurya@users.noreply.github.com>
2024-09-13 15:55:27 +0530

    make it responsive

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import './Item.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Item.css';
 
 const Item = (props) => {
+  const handleClick = () => {
+    window.scrollTo(0, 0);
+  };
+
   return (
-    <div className='item'>
-      <img onClick={window.scrollTo(0,0)} src={props.image} alt="" />
+    <Link to={`/product/${props.id}`} className="item" onClick={handleClick}>
+      <img src={props.image} alt={props.name} />
       <p>{props.name}</p>
       <div className='item-prices'>
         <div className="item-price-new">₹{props.new_price}</div>
-        <div className="item-price-old">₹{props.old_price}</div>
+        {props.old_price && <div className="item-price-old">₹{props.old_price}</div>}
       </div>
-    </div>
-  )
-}
+    </Link>
+  );
+};
 
-export default Item
+export default Item;
